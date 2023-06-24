7390408f108ff71c61434072995deb59e0d052e9
Stephen OFlynn <soflynn@tcd.ie>
2023-06-24 16:06:32 +0000

    display form sumbit message - fix display error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.getElementById("button-one").addEventListener("submit", function(event) {
+document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
     document.getElementById("message").textContent = "Form submitted!";
 });
