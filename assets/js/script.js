88acf1d750e66753dc7c67a76b98742b6a14f02e
Stephen OFlynn <soflynn@tcd.ie>
2023-06-28 06:22:09 +0000

    add markup to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// adding onclick to display email submitted message on register page
+
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
     document.getElementById("message").textContent = "Email submitted! Please refresh this page if you wish to resubmit a different email.";
